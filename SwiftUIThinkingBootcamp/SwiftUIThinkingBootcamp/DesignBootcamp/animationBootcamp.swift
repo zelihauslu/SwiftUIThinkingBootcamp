d896846c82128a45d69806e25c7173f87d8a9127
Zeliha Uslu <74205513+zelihauslu@users.noreply.github.com>
2022-05-04 16:18:42 +0300

    Add files via upload

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/DesignBootcamp/animationBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  animationBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Zeliha Uslu on 3.05.2022.
+//
+
+import SwiftUI
+
+struct animationBootcamp: View {
+    
+    @State var isAnimated: Bool = false
+
+    
+    var body: some View {
+        ZStack{
+            Color.gray
+                .ignoresSafeArea(.all)
+            
+            VStack{
+                Spacer()
+                Button("Button"){
+                    withAnimation(
+                    ) {
+                        isAnimated.toggle()
+
+                   }
+                }
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(.purple)
+                .cornerRadius(15)
+                RoundedRectangle(cornerRadius: isAnimated ? 50 : 15)
+                    .fill(isAnimated ? .green : .red)
+                    .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300)
+                    .rotationEffect(Angle(radians: isAnimated ? 360 : 0))
+                    .offset(y: isAnimated ? 100 : 0)
+                    .animation(Animation
+                        .default
+                        .repeatForever(autoreverses: true))
+                Spacer()
+
+            }
+        }
+    }
+}
+
+struct animationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        animationBootcamp()
+    }
+}
