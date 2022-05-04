18d092342564cf7423cb43df8e9a9dcebda84a4e
Zeliha Uslu <74205513+zelihauslu@users.noreply.github.com>
2022-05-04 18:41:16 +0300

    Add files via upload

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/DesignBootcamp/TransitionBootcamp.swift
@@ -0,0 +1,45 @@
+//
+//  TransitionBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Zeliha Uslu on 4.05.2022.
+//
+
+import SwiftUI
+
+struct TransitionBootcamp: View {
+    
+    @State var showView : Bool = false
+    
+    var body: some View {
+        ZStack(alignment: .bottom){
+            
+            VStack{
+                Button("Button"){
+                    showView.toggle()
+                }
+                Spacer()
+            }
+           
+            
+            if showView{
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(.red)
+                    .frame(height: UIScreen.main.bounds.height * 0.5)
+                    .transition(.asymmetric(insertion: .move(edge: .bottom), removal: AnyTransition.opacity.animation(.easeInOut)))
+//                    .transition(.move(edge: .bottom))
+//                    .transition(AnyTransition.opacity.animation(.linear))
+                // .transition(AnyTransition.scale.animation(.linear))
+                    .animation(.easeInOut, value: 1.0)
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct TransitionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionBootcamp()
+    }
+}
