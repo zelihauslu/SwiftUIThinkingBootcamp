bc3fe681862fcf9bd98e717c53f67e01335487f9
Zeliha Uslu <74205513+zelihauslu@users.noreply.github.com>
2022-05-20 20:44:42 +0300

    Add files via upload

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcamp/textSelectionBootcamp.swift
@@ -0,0 +1,22 @@
+//
+//  textSelectionBootcamp.swift
+//  SwiftUIThinking-Bootcamp2
+//
+//  Created by Zeliha Uslu on 20.05.2022.
+//
+
+import SwiftUI
+
+struct textSelectionBootcamp: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+        
+    }
+}
+
+struct textSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        textSelectionBootcamp()
+    }
+}
